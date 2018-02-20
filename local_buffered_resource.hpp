a125accd24a28298c5a099e0dd8f7d3a583db4bc
Shaktal <thomas.russell97@googlemail.com>
2018-02-20 19:53:48 +0000

    Added more implementation.

--- local_buffered_resource.hpp
+++ local_buffered_resource.hpp
@@ -1,6 +1,9 @@
 #ifndef LOCAL_BUFFERED_RESOURCE
 #define LOCAL_BUFFERED_RESOURCE
 
+#include <bitset>
+#include <cassert>
+#include <new>
 #include <type_traits>
 
 namespace data_structures {
@@ -11,7 +14,7 @@ public: // Types
     using size_type = std::size_t;
 
 public: // Construction
-    LocalBufferedResource();
+    LocalBufferedResource() = default;
     LocalBufferedResource(const LocalBufferedAllocator&) = delete;
     
 public: // Assignment
@@ -19,9 +22,9 @@ public: // Assignment
 
 public: // Allocation
     [[nodiscard]] constexpr void* allocate(size_type numBytes, 
-        size_type alignment = alignof(std::max_align_t)) noexcept;
+        size_type alignment = alignof(std::max_align_t));
 
-    constexpr void deallocate(void* position, size_type numBytes) noexcept;
+    constexpr void deallocate(void* position, size_type numBytes);
 
 private: // Members
     using BufferType = std::aligned_storage_t<Capacity, Alignment>;
@@ -29,6 +32,50 @@ private: // Members
     BufferType              d_buffer;
     std::bitset<Capacity>   d_byteBook; // Keeps track of which bytes are allocated
 }; 
+
+// =================================================================
+// INLINE DEFINITIONS
+// =================================================================
+// Allocation
+template <std::size_t Capacity, std::size_t Alignment>
+inline constexpr void* LocalBufferedResource<Capacity, Alignment>::allocate(
+    size_type numBytes, size_type alignment)
+{
+    if(numBytes <= (Capacity - d_byteBook.count()))
+    {
+        throw std::bad_alloc("Unable to allocate more memory. "
+        "Buffer does not have enough contiguous space");
+    }
+
+    // Find the first set of numBytes contiguous unset bits
+    // in the bitset
+    for (std::size_t index = 0u; index < Capacity; ++index)
+    {
+        std::size_t numContiguous;
+        for (numContiguous = 0u; numContiguous < (Capacity - numBytes); ++numContiguous)
+        {
+            if (const_cast<const std::bitset<Capacity>>(d_byteBook)[i])
+            {
+                break;
+            }
+        }
+
+        if (numContiguous == numBytes - 1u)
+        {
+            for (numContiguous = 0u; numContiguous < numBytes; ++numContiguous)
+            {
+                d_byteBook.set(index + numContiguous);
+            }
+
+            return reinterpret_cast<void*>(
+                reinterpret_cast<std::byte*>(std::addressof(d_buffer))
+                + index);
+        }
+    }
+
+    throw std::bad_alloc("Unable to allocate more memory. "
+        "Buffer does not have enough contiguous space");
+}
 
 } // close namespace data_structures
 
